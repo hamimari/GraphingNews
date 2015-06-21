83e6e02c0623bae933a6a4110d625c867d9b68a6
nendijuned <nendi.juned15@gmail.com>
2015-06-21 13:36:05 +0700

    add file Test.java

---
+++ src/graph/readfile/Test.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package graph.readfile;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author NendiJuned
+ */
+public class Test {
+    public static void main(String[] args) throws IOException {
+        
+        //ReadFile Document
+//        ReadFileDOC DOC = new ReadFileDOC();
+//        Scanner s = new Scanner(System.in);
+//        String link = s.next();
+//        System.out.println(Arrays.toString(DOC.GetDataFromFilePerBaris(link)));
+//        System.out.println(DOC.GetDataFromFile(link));
+        
+        //Read File TXT
+//        ReadFileTXT TXT = new ReadFileTXT();
+//        Scanner s = new Scanner(System.in);
+//        String link = s.next();
+//        TXT.GetDataFromFile(link);
+        
+        //Read File XLS
+//        ReadFileXLS XLS = new ReadFileXLS();
+//        Scanner s = new Scanner(System.in);
+//        String link = s.next();
+//        XLS.ReadFileXLS(link);
+    }
+}
